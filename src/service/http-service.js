5adb6272462c6ed7b9b444f7f496df188da96908
Nadav <nadavkomo@gmail.com>
2020-11-25 13:13:17 +0200

    create dev

--- src/service/http-service.js
+++ src/service/http-service.js
@@ -1,33 +1,33 @@
 import router from '@/router'
 
 
-const BASE_URL = process.env.NODE_ENV === 'production'
-    ? '/api/'
-    : '//localhost:3030/api/'
+const BASE_URL = process.env.NODE_ENV === 'production' ?
+    '/api/' :
+    '//localhost:3030/api/'
 
 
 import Axios from 'axios';
 var axios = Axios.create({
-    withCredentials: true  // מה הפרוש?
+    withCredentials: true
 });
 
 export default {
-    get(endpoint, data){
+    get(endpoint, data) {
         return ajax(endpoint, 'GET', data)
     },
-    post(endpoint, data){
+    post(endpoint, data) {
         return ajax(endpoint, 'POST', data)
     },
-    put(endpoint, data){
+    put(endpoint, data) {
         return ajax(endpoint, 'PUT', data)
     },
-    delete(endpoint, data){
+    delete(endpoint, data) {
         return ajax(endpoint, 'DELETE', data)
     }
 }
 
 
-async function ajax(endpoint, method='get', data=null) {
+async function ajax(endpoint, method = 'get', data = null) {
     try {
         const res = await axios({
             url: `${BASE_URL}${endpoint}`,
@@ -44,4 +44,3 @@ async function ajax(endpoint, method='get', data=null) {
         throw err;
     }
 }
-
