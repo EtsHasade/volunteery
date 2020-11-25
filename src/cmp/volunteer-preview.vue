36da47efc55f02818f964e39ba0f47faf1932a94
Elhanan Avihail <etsavihail@gmail.com>
2020-11-25 15:35:16 +0200

    conect cmps - volu list, volu preview, import scss

--- src/cmp/volunteer-preview.vue
+++ src/cmp/volunteer-preview.vue
@@ -1,1 +1,17 @@
 
+<template>
+  <li class="volunteer-preview">
+      <img src="@/assets/img/650085_47717403.jpg" alt="">
+  </li>
+</template>
+
+<script>
+export default {
+    name: 'volunteerPreview'
+
+}
+</script>
+
+<style>
+
+</style>
