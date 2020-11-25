7dae697e81cf62a9a0e002dbbf545393291d8f59
Nadav <nadavkomo@gmail.com>
2020-11-25 16:19:40 +0200

    volunteer details2

--- src/views/volunteer-details.vue
+++ src/views/volunteer-details.vue
@@ -1,10 +1,28 @@
 <template>
   <section class="volunteer-details">
-    <img class="volunteer-img" src="" alt="" srcset="" />
+    <img class="volunteer-img" :src="volunteer.imgUrls[0]" alt="" srcset="" />
+    <main>
     <section class="details">
-        <img class="img-organization" src="" alt="" srcset="">
+      <span>{{ volunteer.name }}</span>
+      <section class="mini-org">
+        <img class="img-org" :src="volunteer.byOrg.imgUrl" alt="" />
+        <span>By {{ volunteer.byOrg.name }}</span>
+      </section>
+      <span>{{volunteer.location.country}} {{volunteer.location.address}}</span>
+      <span>{{colunteer.desc}}</span>
+      <section class="reviews">
+        <section class="review" v-for="review in volunteer.review" :key="review._id">
+            <img class="img-review" :src="review.author.imgUrl" alt="" srcset=""> 
+            <span class="name-review">{{review.author.fullName}}</span>
+            <span class="txt-review">{{review.txt}}</span>
+            <span class="review-rate">Rate: {{review.rate}}</span>
+        </section>
+      </section>
+    </section>
+    <section>
         
     </section>
+    </main>
   </section>
 </template>
 
