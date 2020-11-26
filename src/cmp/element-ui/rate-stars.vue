d7176de641010e78bad63a852db947874d6b632c
Elhanan Avihail <etsavihail@gmail.com>
2020-11-26 14:11:38 +0200

    fix stars

--- src/cmp/element-ui/rate-stars.vue
+++ src/cmp/element-ui/rate-stars.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="rate-stars">
     <el-rate
-      v-model="value"
-      
+      v-model="value"  
       show-score
       text-color="#ff9900"
       score-template="{value} points"
@@ -15,22 +14,14 @@
 // disabled
 
 export default {
-  props: {
-    initRate: Number,
-  },
   data() {
     return {
       value: 0
     };
   },
-  created() {
-    this.rate = this.initRate;
-  },
   watch:{
     value: function(newValue){
-      this.$emit('input', newValue)
-      console.log('newValue',newValue);
-      
+      this.$emit('input', newValue)     
     }
   }
 };
