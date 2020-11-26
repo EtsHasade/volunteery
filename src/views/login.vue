ca6d97c5ba87cad99f09c9c4866db30faaff7238
yotambarak <yotam.barakk@gmail.com>
2020-11-26 14:53:08 +0200

    add login

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,24 +1,28 @@
 <template>
     <div class="login-page">
+        <h2>{{msg}}</h2>
         <section class="login">
-        <h2>Login</h2>
-            <input type="text" name="username" v-model="loginCred.username" placeholder="Username" />
-            <input type="password" name="password" v-model="loginCred.password" placeholder="Password" />
-            <button type="button" @click="login">Login</button>
-        </section>
+            <h3>Login</h3>
+            <form @submit.prevent="login">
+                <input type="text" name="username" v-model="loginCred.username" placeholder="Username" />
+                <input type="password" name="password" v-model="loginCred.password" placeholder="Password" />
+                <button>Login</button>
+            </form>
+        </section>        
         <section class="signup">
-            <h2>Sign-up</h2>
-            <input type="text" name="username" v-model="signupCred.username" placeholder="Username" />
-            <input type="password" name="password" v-model="signupCred.password" placeholder="Password" />
-            <input type="email" name="email" v-model="signupCred.email" placeholder="baba@gmail.com" />
-            <button type="button" @click="signup">Sign</button>
+            <h3>Sign-up</h3>
+            <form @submit.prevent="signup">
+                <input type="text" name="username" v-model="signupCred.username" placeholder="Username" />
+                <input type="password" name="password" v-model="signupCred.password" placeholder="Password" />
+                <input type="email" name="email" v-model="signupCred.email" placeholder="baba@gmail.com" />
+                <button type="button" @click="signup">Sign</button>
+          </form>
         </section>
     </div>
     
 </template>
 
 <script>
-    import userService from '../service/user-service.js';
 
     export default {
         name: 'Login',
@@ -26,28 +30,33 @@
             return {
                 loginCred: {},
                 signupCred: {},
+                 msg: '',
             }
         },
         methods: {
-            login() {
-                if(!this.loginCred.username && !this.loginCred.password) {
-                    if(this.loginCred.username == "" && this.loginCred.password == "") {
-                        //check on usert list
-                        this.$emit("authenticated", true);
-                        this.$router.replace({ name: "secure" });
-                    } else {
-                        console.log("The username or password is incorrect");
+            async login() {
+                if(this.loginCred.username && this.loginCred.password) {
+                    console.table('this.loginCred',this.loginCred);
+                    try {
+                        await this.$store.dispatch({ type: 'login', userCred: this.loginCred })
+                        this.loginCred = {};
+                        this.msg = 'loging now';
+                    } catch (err) {
+                        this.msg = 'Your user/password wrong, please try again.';
                     }
                 } else {
-                    console.log("A username and password must be present");
+                    console.table('this.loginCred',this.loginCred);
+                    return this.msg = 'A username and password must be present';
                 }
             },
             signup(){
-                 if(!this.signupCred.username && !this.signupCred.password && !this.signupCred.email) {
-                    userService.signup(this.signupCred);
-                    console.log("Welcome!");
+                 if(this.signupCred.username && this.signupCred.password && this.signupCred.email) {
+                    this.$store.dispatch({ type: 'signup', userCred: this.signupCred });
+                    this.signupCred = {};
+                    return this.msg = 'Welcome!';
             } else {
-                    console.log("The username or password or email is incorrect");
+                    console.table('this.signupCred',this.signupCred);
+                    return this.msg = 'Some input is incorrect';
                 }
             }
          }
@@ -60,7 +69,15 @@
         border: 1px solid #CCCCCC;
         background-color: #FFFFFF;
         margin: auto;
-        margin-top: 200px;
+        margin-top: 20px;
         padding: 20px;
     }
+    h2{
+        margin-top: 10px;
+        padding: 5px;
+        border: solid 2px black;
+        background-color: #69BD42;
+        width: 300px;    
+    }
+
 </style>
