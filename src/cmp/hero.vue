2645ff1d15a880581db8f1cf5a6d1f82f4c9aae7
Elhanan Avihail <etsavihail@gmail.com>
2020-12-01 21:54:22 +0200

    hiden hero text in ather pages

--- src/cmp/hero.vue
+++ src/cmp/hero.vue
@@ -4,10 +4,11 @@
       <h1>Looking to do good?</h1>
       <h3>Togather, we build better world</h3>
       <p>
-        Want to volunteer in your own style and according to your unique talents?
+        Want to volunteer in your own style and according to your unique
+        talents?
       </p>
       <p>We invite you to choose the volunteering that best suits you.</p>
-      <a  class="call-to-action" href=""><h4>See our suggestions</h4></a>
+      <h4 ref="scrollToList" class="call-to-action" @click="scrollMeTo('scrollToList')">See our suggestions</h4>
     </div>
   </div>
 </template>
@@ -15,6 +16,13 @@
 <script>
 export default {
   name: "hero",
+  methods: {
+    scrollMeTo(refName) {
+      var element = this.$refs[refName];
+      var top = element.offsetTop;
+      window.scrollTo(0, top);
+    },
+  },
 };
 </script>
 
