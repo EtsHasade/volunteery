b2cb9e65fd26eb6492d74bc648ef1e44dc7e914f
Nadav <nadavkomo@gmail.com>
2020-12-02 23:04:20 +0200

    duration

--- src/cmp/eventi-preview.vue
+++ src/cmp/eventi-preview.vue
@@ -1,44 +1,58 @@
 
 
 <template>
-  <li class="eventi-preview card-preview flex-column" v-if="eventi" @click="openDetails">
+  <li
+    class="eventi-preview card-preview flex-column"
+    v-if="eventi"
+    @click="openDetails"
+  >
     <div class="img-squer-container">
-      <img v-show="imgLoad" @load="imgLoad = true" :src="eventi.imgUrls[0]" alt="" />
+      <img
+        v-show="imgLoad"
+        @load="imgLoad = true"
+        :src="eventi.imgUrls[0]"
+        alt=""
+      />
       <div v-if="!imgLoad" class="loading flex center">LOADING...</div>
-      <p class="country preview-card-country">{{eventi.location.country}}</p>
+      <p class="country preview-card-country">{{ eventi.location.country }}</p>
     </div>
     <section v-if="org" class="details-org-section flex align-center">
-        <avatar
-          style="background-position: center; background-size: cover"
-          class="org-logo hover-pointer mr10"
-          :username="org.name"
-          :src="org.logo"
-          :title="org.name"
-          @click.stop="openOrgDetails"
-        />
-        <h3 class="mrg0 card-title">{{eventi.title}}</h3>
+      <avatar
+        style="background-position: center; background-size: cover"
+        class="org-logo hover-pointer mr10"
+        :username="org.name"
+        :src="org.logo"
+        :title="org.name"
+        @click.stop="openOrgDetails"
+      />
+      <h3 class="mrg0 card-title">{{ eventi.title }}</h3>
     </section>
     <section class="mini-details-top flex space-between">
-            <h5 v-if="eventi.reviews.length">
-              <i class="star fas fa-star"></i>
-              {{ eventi.rate }} ({{ eventi.reviews.length }} reviews)
-            </h5>
-            <h5 v-else><i class="star fas fa-star"></i>New</h5> 
-            <h5 class="time">{{ moment(eventi.startAt).format("DD/MM/YYYY") }}</h5>
+      <h5 v-if="eventi.reviews.length">
+        <i class="star fas fa-star"></i>
+        {{ eventi.rate }} ({{ eventi.reviews.length }} reviews)
+      </h5>
+      <h5 v-else><i class="star fas fa-star"></i>New</h5>
+      <h5 class="time">{{ moment(eventi.startAt).format("DD/MM/YYYY") }}</h5>
     </section>
     <section class="mini-details-main flex justify-center">
       <section class="price mr16">
-        <p v-if="eventi.price > 0">${{eventi.price}} per week</p>
+        <p v-if="eventi.price > 0">${{ eventi.price }} per week</p>
         <p v-else>Free, just come!</p>
       </section>
-      <p>Age +{{eventi.fromAge}}</p>
+      <p>Age +{{ eventi.fromAge }}</p>
     </section>
+    <p>{{duration}}</p>
     <div class="preview-details flex-column">
       <p class="card-desc flex-g1">
         {{ eventi.previewDesc || eventi.desc }}
       </p>
       <div class="eventi-floor card-footer flex space-around">
-        <section class="tag flex column center" v-for="(tag, idx) in eventi.tags" :key="idx">
+        <section
+          class="tag flex column center"
+          v-for="(tag, idx) in eventi.tags"
+          :key="idx"
+        >
           <i :class="tagsIcon[tag]"></i>
           <p>{{ tag }}</p>
         </section>
@@ -79,10 +93,19 @@ export default {
   //   },
   // },
   computed: {
-    
     tagsIcon() {
       return this.$store.getters.tagsIcon;
     },
+    duration() {
+      const date1 = this.eventi.startAt;
+      const date2 = this.eventi.endAt;
+      const diffTime = Math.abs(date2 - date1);
+      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+      var msg = `${(diffDays/7).toFixed(0)} weeks` 
+      if(diffDays%7) msg += ` and ${diffDays%7} days`
+      return msg
+
+    }
   },
   methods: {
     async getOrg() {
@@ -94,7 +117,8 @@ export default {
     },
     openOrgDetails() {
       this.$router.push(`/org-details/${this.eventi.byOrg._id}`)
-    }
+    },
+   
   }
 };
 </script>
