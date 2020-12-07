127386b807c77b1be5afd6a0a24e83df5d568b35
Elhanan Avihail <etsavihail@gmail.com>
2020-12-07 14:36:28 +0200

    router f see al

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -7,31 +7,37 @@
     <section v-if="loggedinUser && loggedinUser.favs" class="suggested">
       <section class="flex space-between">
         <h3>Suggested for you</h3>
-        <h3 class="hover-pointer" @click="$router.push('/eventi-app')">Show All</h3>
+        <h3 class="hover-pointer" @click="$router.push('/eventi-app')">
+          Show All
+        </h3>
       </section>
       <eventi-list :eventis="suggestEventis" />
     </section>
-      <section class="flex space-between">
-        <h3>Newest</h3>
-        <h3 class="hover-pointer" @click="$router.push('/eventi-app')">Show All</h3>
-      </section>
+    <section class="flex space-between">
+      <h3>Newest</h3>
+      <h3 class="hover-pointer" @click="$router.push('/eventi-app')">
+        Show All
+      </h3>
+    </section>
     <eventi-list :eventis="newestEventis" />
-      <section class="flex space-between">
-        <h3>Populars</h3>
-        <h3 class="hover-pointer" @click="$router.push('/eventi-app')">Show All</h3>
-      </section>
+    <section class="flex space-between">
+      <h3>Populars</h3>
+      <h3 class="hover-pointer" @click="$router.push('/eventi-app')">
+        Show All
+      </h3>
+    </section>
     <eventi-list :eventis="popularestEventis" />
     <global-info />
   </div>
 </template>
 
 <script>
-import eventiFilter from '../cmp/eventi-filter.vue';
+import eventiFilter from "../cmp/eventi-filter.vue";
 // @ is an alias to /src
 
-import eventiList from '../cmp/eventi-list'
+import eventiList from "../cmp/eventi-list";
 // import eventiPreview from '../cmp/eventi-preview';
-import globalInfo from '../cmp/global-info';
+import globalInfo from "../cmp/global-info";
 // import socketService from '../service/socket-service.js'
 
 // import orgPreview from '../cmp/org-preview'
@@ -39,60 +45,68 @@ import globalInfo from '../cmp/global-info';
 // import { orgService } from '../service/org-service.js'
 // import { userService } from '../service/user-service.js'
 export default {
-  name: 'homePage',
+  name: "homePage",
   computed: {
     loggedinUser() {
-      return this.$store.getters.loggedinUser
+      return this.$store.getters.loggedinUser;
     },
     newestEventis() {
-      var eventis = JSON.parse(JSON.stringify(this.$store.getters.eventisForDisplay))
-      eventis.sort((a, b) => (a.createdAt > b.createdAt) ? 1 : -1);
-      return eventis.splice(0, 4)
+      var eventis = JSON.parse(
+        JSON.stringify(this.$store.getters.eventisForDisplay)
+      );
+      eventis.sort((a, b) => (a.createdAt > b.createdAt ? 1 : -1));
+      return eventis.splice(0, 4);
     },
     popularestEventis() {
-      var eventis = JSON.parse(JSON.stringify(this.$store.getters.eventisForDisplay))
-      eventis.sort((a, b) => (a.rate > b.rate) ? -1 : ((b.rate > a.rate) ? 1 : 0));
-      return eventis.splice(0, 4)
+      var eventis = JSON.parse(
+        JSON.stringify(this.$store.getters.eventisForDisplay)
+      );
+      eventis.sort((a, b) => (a.rate > b.rate ? -1 : b.rate > a.rate ? 1 : 0));
+      return eventis.splice(0, 4);
     },
     suggestEventis() {
       // if(!this.loggedinUser || !this.loggedinUser.favs) return
-      var suggestEventis = []
-      var suggestEventisArrays = []
-      const favs = this.$store.getters.loggedinUser.favs
-      var eventis = JSON.parse(JSON.stringify(this.$store.getters.eventisForDisplay))
-      eventis.sort((a, b) => (a.rate > b.rate) ? -1 : ((b.rate > a.rate) ? 1 : 0));
-      [...favs].forEach(fav => {
+      var suggestEventis = [];
+      var suggestEventisArrays = [];
+      const favs = this.$store.getters.loggedinUser.favs;
+      var eventis = JSON.parse(
+        JSON.stringify(this.$store.getters.eventisForDisplay)
+      );
+      eventis.sort((a, b) => (a.rate > b.rate ? -1 : b.rate > a.rate ? 1 : 0));
+      [...favs].forEach((fav) => {
         let favEventis = [];
-        [...eventis].forEach(eventi => {
-          eventi.tags.forEach(eventTag => {
+        [...eventis].forEach((eventi) => {
+          eventi.tags.forEach((eventTag) => {
             if (eventTag === fav) {
-              favEventis.push(eventi)
+              favEventis.push(eventi);
             }
-          })
-        })
-        suggestEventisArrays.push(JSON.parse(JSON.stringify(favEventis)))
-      })
+          });
+        });
+        suggestEventisArrays.push(JSON.parse(JSON.stringify(favEventis)));
+      });
 
-      const arr = [0, 1, 2, 3]
+      const arr = [0, 1, 2, 3];
       arr.forEach((num) => {
-        var myIdx = num
-        if (myIdx === suggestEventisArrays.length) myIdx = 0
-        if (myIdx - 1 === suggestEventisArrays.length) myIdx = 1
-        suggestEventis.push(suggestEventisArrays[myIdx].shift())
-      })
-      return suggestEventis
-    }
+        var myIdx = num;
+        if (myIdx === suggestEventisArrays.length) myIdx = 0;
+        if (myIdx - 1 === suggestEventisArrays.length) myIdx = 1;
+        suggestEventis.push(suggestEventisArrays[myIdx].shift());
+      });
+      return suggestEventis;
+    },
   },
   methods: {
-    routeToEventiFilter(filterBy){
-      if (filterBy.byKey.values.length){
-        this.$router.push(`/eventi-app/?term=${filterBy.byText}&key=${filterBy.byKey.key}&values=${filterBy.byKey.values}`)
-        }
-    }
+    routeToEventiFilter(filterBy) {
+      if (filterBy.byKey.values.length) {
+        this.$router.push(
+          `/eventi-app/?term=${filterBy.byText}&key=${filterBy.byKey.key}&values=${filterBy.byKey.values}`
+        );
+      }
+    },
   },
   created() {
-    this.$store.dispatch({ type: 'setEventis' });
-    document.body.classList.add('hero-page');
+    this.$store.dispatch({ type: "setEventis" });
+    document.body.classList.add("hero-page");
     // socketService.on("updatesEventi", (eventi) => {
     //   console.log('check');
     //   if(this.$store.getters.loggedinUser.org._id === eventi.byOrg._id) {
@@ -101,18 +115,18 @@ export default {
     //       message: `someone review on ${eventi.title}`,
     //       type: "success",
     //       duration: 3000,
-    //     });      
+    //     });
     //   }
     // })
   },
   destroyed() {
-    document.body.classList.remove('hero-page');
+    document.body.classList.remove("hero-page");
   },
   components: {
     eventiList,
     globalInfo,
     eventiFilter,
     // orgPreview
-  }
-}
+  },
+};
 </script>
